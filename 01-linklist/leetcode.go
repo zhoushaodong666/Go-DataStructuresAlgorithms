f606dcdfc0859ad53758484dc9589164d473b7b9
zhoushaodong666 <1138894663@qq.com>
2019-07-24 18:02:45 +0800

    leetcode 237

---
+++ 01-linklist/leetcode.go
@@ -0,0 +1,25 @@
+package _1_linklist
+
+/*
+Project:
+Author: zhoushaodong
+Email: 1138894663@qq.com
+Github: https://github.com/zhoushaodong666
+Date: 2019/7/24
+Time: 17:44
+*/
+
+//LeetCode 237
+//Definition for singly-linked list.
+
+//type ListNode struct {
+//    Val int
+//    Next *ListNode
+//}
+
+/*
+func deleteNode(node *ListNode) {
+	node.Val=node.Next.Val
+	node.Next=node.Next.Next
+}
+*/

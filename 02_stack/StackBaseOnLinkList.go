b562693ab79ca2cfb1e6d2b5fd6f37fd5317cd82
zhoushaodong <1138894663@qq.com>
2019-08-18 20:33:26 +0800

    链栈的实现

--- 02_stack/StackBaseOnLinkList.go
+++ 02_stack/StackBaseOnLinkList.go
@@ -1,11 +1,55 @@
 package _2_stack
 
-type NodeStack struct {
+import "fmt"
+
+type Node struct {
 	data interface{}
-	next *NodeStack
+	next *Node
 }
 
 type LinkListStack struct {
-	head *NodeStack
-	top  int
+	topNode *Node
+}
+
+func InitLinkListStack() *LinkListStack {
+	return &LinkListStack{nil}
+}
+
+func (this *LinkListStack) IsEmpty() bool {
+	return this.topNode == nil
+}
+
+func (this *LinkListStack) Push(v interface{}) {
+	this.topNode = &Node{v, this.topNode}
+}
+
+func (this *LinkListStack) Pop() interface{} {
+	if this.IsEmpty() {
+		return nil
+	}
+	val := this.topNode.data
+	this.topNode = this.topNode.next
+	return val
+}
+
+func (this *LinkListStack) Top() interface{} {
+	if this.IsEmpty() {
+		return nil
+	}
+	return this.topNode.data
+}
+
+func (this *LinkListStack) Flush() {
+	this.topNode = nil
+}
+
+func (this *LinkListStack) Print() {
+	if this.IsEmpty() {
+		fmt.Println("empty stack")
+	}
+	cur := this.topNode
+	for cur != nil {
+		fmt.Println(cur.data)
+		cur = cur.next
+	}
 }

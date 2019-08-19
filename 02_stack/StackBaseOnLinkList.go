a386c76b6fa3fbe80657d843d03df56f93db843c
zhoushaodong <1138894663@qq.com>
2019-08-19 17:13:50 +0800

    链栈的实现

--- 02_stack/StackBaseOnLinkList.go
+++ 02_stack/StackBaseOnLinkList.go
@@ -2,27 +2,33 @@ package _2_stack
 
 import "fmt"
 
+// 定义链表节点结构体
 type Node struct {
 	data interface{}
 	next *Node
 }
 
+// 定义链栈结构体
 type LinkListStack struct {
 	topNode *Node
 }
 
+// 初始化链栈
 func InitLinkListStack() *LinkListStack {
 	return &LinkListStack{nil}
 }
 
+// 判断链栈是否为空
 func (this *LinkListStack) IsEmpty() bool {
 	return this.topNode == nil
 }
 
+// 入栈
 func (this *LinkListStack) Push(v interface{}) {
 	this.topNode = &Node{v, this.topNode}
 }
 
+// 出栈
 func (this *LinkListStack) Pop() interface{} {
 	if this.IsEmpty() {
 		return nil
@@ -32,6 +38,7 @@ func (this *LinkListStack) Pop() interface{} {
 	return val
 }
 
+// 获取栈顶元素值
 func (this *LinkListStack) Top() interface{} {
 	if this.IsEmpty() {
 		return nil
@@ -39,10 +46,12 @@ func (this *LinkListStack) Top() interface{} {
 	return this.topNode.data
 }
 
+// 链栈置空
 func (this *LinkListStack) Flush() {
 	this.topNode = nil
 }
 
+// 打印链栈
 func (this *LinkListStack) Print() {
 	if this.IsEmpty() {
 		fmt.Println("empty stack")
